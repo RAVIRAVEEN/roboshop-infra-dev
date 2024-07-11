1f3da77063ddc4bd4cfadda1e5074f1e0a3d157c
localstar1996 <localstar1996@gmail.com>
2024-07-11 10:09:07 +0530

    first commit

--- 04-databases/bootstrap.sh
+++ 04-databases/bootstrap.sh
@@ -3,6 +3,6 @@
 component=$1
 envinorment=$2  #don't use env, because its a reserve word in linux
 
-yum install python 3.12-devel python 3.12-pip -y
-pip3.12 install ansible botocore boto3
+yum install python 3.11-devel python 3.11-pip -y
+pip3.11 install ansible botocore boto3
 ansible-pull -U https://github.com/RAVIRAVEEN/roboshop-ansible-roles-tf.git -e component=$component -e env=$envinorment main.tf.yaml
